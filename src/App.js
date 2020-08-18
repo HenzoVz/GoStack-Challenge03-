3b27301bbb7d6db42bc5b511033fbc25e0dda553
henzovz <murilohezo@gmail.com>
2020-08-18 14:47:24 -0300

    desafio 03 finalizado

--- src/App.js
+++ src/App.js
@@ -1,29 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [title, setTitle] = useState("");
+  const [repositories, setRepositories] = useState([]);
+
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: title,
+      url: `github.com/Challenges/${title}`,
+      techs: ["NodeJS", "ReactJS"]
+    });
+
+    const repository = response.data;
+    console.log(repository)
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    const new_repositories = repositories.filter((repository) => repository.id !== id);
+    
+    setRepositories(new_repositories);
   }
 
   return (
     <div>
+      <form onSubmit={handleAddRepository}>
+        <input
+          type="text"
+          autoComplete="off"
+          autoFocus
+          value={title}
+          onChange = {event => setTitle(event.target.value)}
+          placeholder="Repository Name"
+        />
+      </form>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        { repositories.map(repository => 
+        <li key={repository.id}>
+          {repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        )}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button type="submit" onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
